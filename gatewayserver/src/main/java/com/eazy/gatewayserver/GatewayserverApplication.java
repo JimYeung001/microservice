11f95a838a81d3b31bb9adaab1e708fe6f5f979e
Jim Yeung <qzy114@gmail.com>
2024-01-06 21:39:26 -0500

    add route

--- gatewayserver/src/main/java/com/eazy/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/eazy/gatewayserver/GatewayserverApplication.java
@@ -2,6 +2,9 @@ package com.eazy.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayserverApplication {
@@ -10,4 +13,18 @@ public class GatewayserverApplication {
 		SpringApplication.run(GatewayserverApplication.class, args);
 	}
 
+	
+	@Bean
+	public RouteLocator eazyRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+		return routeLocatorBuilder.routes()
+				.route(p -> p.path("/easy/accounts/**")
+						.filters(f->f.rewritePath("/easy/accounts/(?<segment>.*)", "/${segment}"))
+						.uri("lb://ACCOUNTS"))
+				.route(p -> p.path("/easy/loans/**")
+						.filters(f->f.rewritePath("/easy/loans/(?<segment>.*)", "/${segment}"))
+						.uri("lb://LOANS"))
+				.route(p -> p.path("/easy/cards/**")
+						.filters(f->f.rewritePath("/easy/cards/(?<segment>.*)", "/${segment}"))
+						.uri("lb://CARDS")).build();
+	}
 }
